2d4d935d3fd85f3e20405c0093bda9baeb3607d9
dohuy0708 <dohuy22314@gmail.com>
2025-04-10 10:44:13 +0700

    feat : page pagination

--- src/main/java/com/Harum/Harum/Controllers/PostReportController.java
+++ src/main/java/com/Harum/Harum/Controllers/PostReportController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/post_reports/")
+@RequestMapping("/api/post_reports")
 public class PostReportController {
 
     @Autowired
